0caba4646deb24b3bd17f37a69c0342245b4a133
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-05-10 19:11:15 +0700

    update mobile navigation link function

--- components/landing_page_layout/app-shell.tsx
+++ components/landing_page_layout/app-shell.tsx
@@ -1,32 +1,52 @@
 'use client';
-
 // React import
-
 // Mantine import
 import { AppShell, Burger, Group, Button } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 // import { useDisclosure } from '@mantine/hooks/lib/index.mjs';
-
 // Next JS import
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { useRouter } from 'next/navigation';
 // Components import
 import { PrimaryButton } from './buttons';
 import { OutlineButton } from './buttons';
 import Container from './container';
-
 // CSS import
 import classes from './app-shell.module.css';
-
 // Image Import
 import appLogo from '@/public/images/app-logo.png';
-
 // component import
 import Footer from './footer';
 
+const links = [
+  {
+    title: 'Home',
+    href: '/#hero',
+  },
+  {
+    title: 'About',
+    href: '/#about',
+  },
+  {
+    title: 'Features',
+    href: '/#features',
+  },
+  {
+    title: 'Reviews',
+    href: '/#reviews',
+  },
+];
+
 export function LandingPageAppShell({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   const [opened, { toggle }] = useDisclosure();
+
+  const handleLinkPress = (href: string) => {
+    toggle();
+    router.push(href);
+  };
 
   return (
     <AppShell
@@ -64,30 +84,15 @@ export function LandingPageAppShell({ children }: { children: React.ReactNode })
               </div>
 
               <div className='hidden sm:flex gap-4 lg:gap-10 sm:items-center'>
-                <Link
-                  href='/#hero'
-                  className='text-sm border-b border-transparent hover:border-white transition-all'
-                >
-                  Home
-                </Link>
-                <Link
-                  href='/#about'
-                  className='text-sm border-b border-transparent hover:border-white transition-all'
-                >
-                  About
-                </Link>
-                <Link
-                  href='/#features'
-                  className='text-sm border-b border-transparent hover:border-white transition-all'
-                >
-                  Features
-                </Link>
-                <Link
-                  href='/#reviews'
-                  className='text-sm border-b border-transparent hover:border-white transition-all'
-                >
-                  Reviews
-                </Link>
+                {links.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link.href}
+                    className='text-sm border-b border-transparent hover:border-white transition-all'
+                  >
+                    {link.title}
+                  </Link>
+                ))}
               </div>
 
               {/* <Group
@@ -122,39 +127,17 @@ export function LandingPageAppShell({ children }: { children: React.ReactNode })
         px='md'
         className={classes.navbar}
       >
-        <div className='flex flex-col gap-6'>
-          <div>
-            <Link
-              href='/#hero'
-              className='pb-1 text-sm border-b border-transparent hover:border-white transition-all'
-            >
-              Home
-            </Link>
-          </div>
-          <div>
-            <Link
-              href='/#about'
-              className='pb-1 text-sm border-b border-transparent hover:border-white transition-all'
-            >
-              About
-            </Link>
-          </div>
-          <div>
-            <Link
-              href='/#features'
-              className='pb-1 text-sm border-b border-transparent hover:border-white transition-all'
-            >
-              Features
-            </Link>
-          </div>
-          <div>
-            <Link
-              href='/#reviews'
-              className='pb-1 text-sm border-b border-transparent hover:border-white transition-all'
-            >
-              Reviews
-            </Link>
-          </div>
+        <div className='flex flex-col gap-6 ps-2'>
+          {links.map((link, index) => (
+            <div key={index}>
+              <button
+                onClick={() => handleLinkPress(link.href)}
+                className='pb-1 text-sm border-b border-transparent hover:border-white transition-all'
+              >
+                {link.title}
+              </button>
+            </div>
+          ))}
           {/* <div className='grid grid-cols-2 gap-4 mt-4'>
             <OutlineButton>Daftar</OutlineButton>
             <PrimaryButton>Masuk</PrimaryButton>
--- components/landing_page_layout/review-carousel.tsx
+++ components/landing_page_layout/review-carousel.tsx
@@ -10,27 +10,37 @@ import { Carousel } from '@mantine/carousel';
 
 // images import
 import reviewPhoto from '@/public/images/review-photo.png';
+import aaronPhoto from '@/public/images/aaron.png';
+import bitusPhoto from '@/public/images/bitus.png';
+import ramaPhoto from '@/public/images/rama.png';
+import firaPhoto from '@/public/images/fira.png';
 
 // component import
 import Container from './container';
 
 const reviews = [
   {
-    image: reviewPhoto,
-    review: 'Sistem aplikasi yang diberikan sangat baik dan mudah untuk dimanfaatkan dalam mengelola waktu tidur saya',
+    image: aaronPhoto,
+    review: 'Yang saya sukai dari aplikasi ini adalah disini kita bisa melakukan tracking seberapa bagus kualitas tidur kita.',
+    name: 'Aaron Febrian',
+    job: 'Mahasiswa PENS',
+  },
+  {
+    image: ramaPhoto,
+    review: 'Tampilan aplikasinya bagus. Dengan adanya keterangan juga membantu pemahaman penggunaan aplikasi.',
     name: 'M. Ainur Ramadhan',
     job: 'Mahasiswa PENS',
   },
   {
-    image: reviewPhoto,
-    review: 'Sistem aplikasi yang diberikan sangat baik dan mudah untuk dimanfaatkan dalam mengelola waktu tidur saya',
-    name: 'Hadyar Supriyanto',
+    image: bitusPhoto,
+    review: 'Saya suka dengan ui aplikasinya karena warnanya pas dan tampilannya yang minimalis.',
+    name: 'Nabilatus Syaadah',
     job: 'Mahasiswa PENS',
   },
   {
-    image: reviewPhoto,
-    review: 'Sistem aplikasi yang diberikan sangat baik dan mudah untuk dimanfaatkan dalam mengelola waktu tidur saya',
-    name: 'Nabilatus Syaadah',
+    image: firaPhoto,
+    review: 'Fungsionalitas aplikasinya bagus. Aplikasi ini akan sangat membantu aktivitas saya.',
+    name: 'Alfira Putri',
     job: 'Mahasiswa PENS',
   },
 ];
@@ -54,10 +64,11 @@ const ReviewCarousel = () => {
                   height={150}
                   // blurDataURL="data:..." automatically provided
                   placeholder='blur'
+                  className='rounded-full'
                 />
               </div>
               <div className='text-center md:text-start'>
-                <p className='text-lg'>{review.review}</p>
+                <p className='text-lg'>&ldquo;{review.review}&rdquo;</p>
                 <p className='font-semibold mt-2'>{review.name}</p>
                 <p className=''>{review.job}</p>
               </div>
